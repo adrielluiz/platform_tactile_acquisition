82cb1f01f8c4e2f02e9dbce8b7add8aeef3b38cb
adrielmarques <adriel.marques@ufu.br>
2023-08-08 12:20:14 -0300

    Adding image

--- FIRMWARE/plataform_tact/include/cmd.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-
-uint32_t cmd_parse_args(uint8_t * const cmdline, uint32_t size, uint32_t *argc, uint8_t *argv[], uint32_t max_args);
-void cmd_proc(uint8_t * const cmdline, uint32_t size);
-
